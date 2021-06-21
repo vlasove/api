6e2bad6fa40d829f5b3914aa4c914c67115dd100
vlasove <evgeny_vlasov@yahoo.com>
2021-06-21 19:02:33 +0300

    before refactor

--- internal/app/model/validations.go
+++ internal/app/model/validations.go
@@ -2,6 +2,7 @@ package model
 
 import validation "github.com/go-ozzo/ozzo-validation"
 
+// RequiredIf ...
 func RequiredIf(cond bool) validation.RuleFunc {
 	return func(value interface{}) error {
 		if cond {
